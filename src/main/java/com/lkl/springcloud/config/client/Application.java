ca27a9e11aa9208681167591d9047c2b728fbaa8
liuyongzhao <liuyongzhao@supcon.com>
2017-12-05 10:27:02 +0800

    change some config

--- src/main/java/com/lkl/springcloud/config/client/Application.java
+++ src/main/java/com/lkl/springcloud/config/client/Application.java
@@ -3,25 +3,19 @@ package com.lkl.springcloud.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by lyz on 17/12/04.
- */
 @EnableAutoConfiguration
-@ComponentScan
 @RestController
-@RefreshScope
 public class Application {
 
-    @Value("${name:World!}") String name ;
+    @Value("${foo}") 
+    String foo ;
 
-    @RequestMapping("/")
-    public String home(){
-        return "Hello " + name;
+    @RequestMapping(value="/hi")
+    public String hi(){
+        return "Hello " + foo;
     }
 
 
